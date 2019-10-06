64da21a2fb691fe3bba109deb61be824aada8dad
Alan69 <gameg1676@gmail.com>
2019-10-06 12:24:02 +0600

    first commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource AudioSource;
+    public GameObject bullet;
+    public Transform firePoint;
+
+    bool shoot = false; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            shoot = true;
+            animator.SetBool("isShooting", true);
+            AudioSource.Play();
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            shoot = false;
+            animator.SetBool("isShooting", false);
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+
+    }
+}
---
+++ Assets/Scripts/flip.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flip : MonoBehaviour
+{
+    public Transform FirePointFlip;
+    
+    void Update()
+    {
+        FirePointFlip.rotation = transform.rotation;
+    }
+    
+}
